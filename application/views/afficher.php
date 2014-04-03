0c498e2730cdf9f8bcb9fa8013d8a4f7612a9ef6
Laour valentin <valentin_laour@ens.univ-artois.fr>
2014-04-04 00:01:18 +0200

    1) Inscription à un trajet 2) Affichage des personnes qui participent à un trajet

--- application/views/afficher.php
+++ application/views/afficher.php
@@ -1,17 +1,39 @@
 
 <div class="container_page">
     <div class="container">
-        <div id="corps">
+        <div class="corps">
             <h3>AFFICHAGE DU TRAJET SELECTIONNE</h3>
-            <h4><?php echo $villeDepart." - Iut de Lens";?></h4>
+            <h4><?php echo $villeDepart." -> Iut de Lens";?></h4>
             <h4><?php echo "Trajet proposé par ".$nomconducteur;?></h4>
-            <h5><a href="">S'inscrire à ce trajet ?</a></h5>
+            <form>
+                </br>
+                <h5><a href="<?php echo site_url('affichertrajet/sinscrire/'.$id);?>">S'inscrire à ce trajet ?</a></h5>
+                </br>
+            </form>
             <div id="directionsDiv"></div>
             <div id="map_canvas"></div>
-
             <?php echo $map['js']; ?>
                 <?php echo $map['html'];?>
-            </div>
+        </div>
+        <div class="info">
+            <h4>Ce trajet vous intéresse ?</h4>
+            <hr>
+            <p Style='text-align:justify;'>
+                Vous pouvez vous y inscrire en cliquant sur "S'inscrire a ce trajet"
+            </p>
+        </div>
+        <div class="info">
+            <h4>Qui participe à ce trajet ?</h4>
+            <hr>
+            <p Style='text-align:justify;'>
+                <?php
+                for ($i = 1 ; $i<$nbParticipant+1;$i++){
+                    $passager = "passager".$i;
+                    echo eval('return $'. $passager . ';');
+                    echo "</br>";
+                }
+                ?>
+            </p>
         </div>
     </div>
 </div>
