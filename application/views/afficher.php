136818563404a7b604cf2947c87f73eef989e4c9
Dolens <vlaour@gmail.com>
2014-04-03 02:38:06 +0200

    Version 4 : Itinéraires, Kilométrages et marqueurs départ/destination.
    JE SUIS FATIGUE.

--- application/views/afficher.php
+++ application/views/afficher.php
@@ -1,10 +1,13 @@
+
 <div class="container_page">
     <div class="container">
         <div id="corps">
             <h3>AFFICHAGE DU TRAJET SELECTIONNE</h3>
             <h4><?php echo $villeDepart." - Iut de Lens";?></h4>
-            <div class="map">
-                <?php echo $map['js']; ?>
+            <div id="directionsDiv"></div>
+            <div id="map_canvas"></div>
+
+            <?php echo $map['js']; ?>
                 <?php echo $map['html'];?>
             </div>
         </div>
